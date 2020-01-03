bfbee2f7fc75f14d81f6874987500e9aa2fbe42a
Zhixing Zhang <me@neoto.xin>
2020-01-04 02:46:48 +0800

    Initial Commit

---
+++ RayHub/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RayHub
+//
+//  Created by Zhixing Zhang on 1/4/20.
+//  Copyright © 2020 Zhixing Zhang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
